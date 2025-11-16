cf97ec7fe76a06e12c8b7fc20f92629c5d4dc183
신승원 <crabhead2797@gmail.com>
2025-11-16 23:18:48 +0900

    all fix2797

--- geo/utils.py
+++ geo/utils.py
@@ -1,12 +1,12 @@
 import math
 
 def pythagoras(a, b):
-    # 피타고라스 정리
+
     c = math.sqrt(a**2 + b**2)
     return c
 
 def circle(r):
-    # 정밀도 문제 해결을 위해 고정 PI 값 사용
+    
     PI = 3.141592653589793
     area = PI * r**2
     return area
--- tester.py
+++ tester.py
@@ -1,4 +1,3 @@
-# tester.py
 from geo import pythagoras, circle 
 result = pythagoras(3,4)
 result2 = circle(7)
