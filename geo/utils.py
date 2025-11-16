9736dbeecf394d1c58067da0bcce6fc857b346e7
신승원 <crabhead2797@gmail.com>
2025-11-16 22:04:41 +0900

    delete y in utils.py

--- geo/utils.py
+++ geo/utils.py
@@ -4,7 +4,7 @@ def pythagoras(a, b):
     # 직각삼각형 빗변 길이 계산: c = sqrt(a**2 + b**2)
     c = math.sqrt(a**2 + b**2)
     return c
-y
+
 def circle(r):
     # 원의 넓이 계산: Area = pi * r**2
     area = math.pi * r**2
